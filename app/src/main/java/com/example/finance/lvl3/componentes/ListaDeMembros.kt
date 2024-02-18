62b21192433988698d791458258a847f6a43d035
Ciro Roberto <ciro.rob2002@gmail.com>
2024-02-18 20:27:46 -0300

    Mudança lista de cards para usar ElevatedCard

--- app/src/main/java/com/example/finance/lvl3/componentes/ListaDeMembros.kt
+++ app/src/main/java/com/example/finance/lvl3/componentes/ListaDeMembros.kt
@@ -1,24 +1,20 @@
 package com.example.finance.lvl3.componentes
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -30,22 +26,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.finance.lvl1.Casa
 import com.example.finance.lvl1.Login
 import com.example.finance.lvl2.Login.testeCadastro
-import com.example.finance.ui.theme.backgroundDark
 import com.example.finance.ui.theme.backgroundLista
-import com.example.finance.ui.theme.contentBackground
 
 @Composable
-fun listaDeMembros(casa: Casa) {
+fun ListaDeMembros(casa: Casa) {
     var expandirConteudo by remember { mutableStateOf(true) }
 
 
@@ -79,9 +68,9 @@ fun listaDeMembros(casa: Casa) {
                 .fillMaxWidth()){
 
             if(expandirConteudo){
-                listaDetalhada(casa = casa)
+                ListaDetalhada(casa = casa)
             }else{
-                listaSimples(casa = casa)
+                ListaSimples(casa = casa)
             }
         }
     }
@@ -89,18 +78,22 @@ fun listaDeMembros(casa: Casa) {
 }
 
 @Composable
-private fun listaSimples(casa: Casa){
+private fun ListaSimples(casa: Casa){
     LazyRow(modifier = Modifier
     ){
         items(casa.moradores){morador->
-            Card(
-                shape = RoundedCornerShape(8.dp),
+            ElevatedCard(
+                shape = RoundedCornerShape(4.dp),
                 modifier = Modifier
-                    .padding(3.dp)) {
+                    .padding(horizontal = 4.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                ) {
                 Text(
                     text = morador.nome,
                     modifier = Modifier
-                        .padding(2.dp),
+                        .padding(6.dp),
                     style = MaterialTheme.typography.bodyLarge)
             }
         }
@@ -108,14 +101,18 @@ private fun listaSimples(casa: Casa){
 }
 
 @Composable
-private fun listaDetalhada(casa: Casa){
+private fun ListaDetalhada(casa: Casa){
     LazyRow(modifier = Modifier
     ){
         items(casa.moradores){morador->
-            Card(shape = RoundedCornerShape(6.dp),
+            ElevatedCard(
+                shape = RoundedCornerShape(6.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
                 modifier = Modifier
-                    .padding(horizontal = 12.dp)) {
-                Column (modifier = Modifier.padding(4.dp)){
+                    .padding(horizontal = 6.dp)) {
+                Column (modifier = Modifier.padding(6.dp)){
                     Text(text = morador.nome,
                         modifier = Modifier
                             .padding(2.dp),
@@ -137,8 +134,8 @@ private fun listaDetalhada(casa: Casa){
 
 @Preview
 @Composable
-fun listaDeMembrosPreview() {
+fun ListaDeMembrosPreview() {
     testeCadastro()
-    listaDeMembros(Login.getCasaLogada())
+    ListaDeMembros(Login.getCasaLogada())
 
 }
