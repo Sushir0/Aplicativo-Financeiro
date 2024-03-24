4dbf6a320f9a6d23e04c8cc54884f9f4870071fa
Ciro Roberto <ciro.rob2002@gmail.com>
2024-03-24 16:30:33 -0300

    Resolve problema de formato errado do getDataString

--- app/src/main/java/com/example/finance/lvl1/Data.kt
+++ app/src/main/java/com/example/finance/lvl1/Data.kt
@@ -6,6 +6,16 @@ class Data (val dia : Int, val mes : Int, val ano : Int){
 
 
     fun getDataString() : String{
-        return "${dia}/${mes}/${ano}"
+        val diaString = if (dia < 10){
+            "0${dia}"
+        }else{
+            "${dia}"
+        }
+        val mesString = if (mes < 10 ){
+            "0${mes}"
+        }else{
+            "${mes}"
+        }
+        return "${diaString}/${mesString}/${ano}"
     }
 }
