17653b93a14d32d32793995563b2335d3f5ff468
Ciro Roberto <ciro.rob2002@gmail.com>
2024-03-28 23:19:32 -0300

    Cria um botão para abrir a formulário de adição de movimentações

---
+++ app/src/main/java/com/example/finance/lvl3/widgets/buttonAdicionar.kt
@@ -0,0 +1,44 @@
+package com.example.finance.lvl3.widgets
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ButtonAdicionar() {
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Icon(
+                Icons.Outlined.AddCircle,
+                contentDescription = "Adicionar movimentações",
+                modifier = Modifier
+                    .size(48.dp)
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ButtonAdicionarPrev() {
+    ButtonAdicionar()
+
+}
