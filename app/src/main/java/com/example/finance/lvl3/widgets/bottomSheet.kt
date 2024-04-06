9114a462f20fb1f92064908c36b5ecbfa181faa9
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-04-06 11:10:39 -0300

    Criação de um Bottom Sheet contendo o formulário de movimentações

---
+++ app/src/main/java/com/example/finance/lvl3/widgets/bottomSheet.kt
@@ -0,0 +1,46 @@
+package com.example.finance.lvl3.widgets
+
+import FormularioMovimentacao
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finance.lvl1.gerarCategoriasBasicas
+import com.example.finance.lvl2.Login.testeCadastro
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(isSheetOpen: Boolean, onDismiss : ()-> Unit){
+
+    if(isSheetOpen){
+        ModalBottomSheet(
+            sheetState = rememberModalBottomSheetState(),
+            onDismissRequest = onDismiss
+        ) {
+            FormularioMovimentacao()
+        }
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+private fun BottomSheetPrev() {
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    gerarCategoriasBasicas()
+    testeCadastro()
+    BottomSheet(isSheetOpen) { isSheetOpen = false }
+}
+
