56f59828724afe4c248c420e9c60c534e901f0de
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-04-06 11:06:12 -0300

    Adiciona função para abrir o Bottom Sheet

--- app/src/main/java/com/example/finance/lvl3/widgets/buttonAdicionar.kt
+++ app/src/main/java/com/example/finance/lvl3/widgets/buttonAdicionar.kt
@@ -9,19 +9,23 @@ import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ButtonAdicionar() {
+fun ButtonAdicionar(openBottomSheetClick : () -> Unit) {
     Box(
         contentAlignment = Alignment.BottomEnd,
         modifier = Modifier.fillMaxWidth()
     ){
         IconButton(
-            onClick = { /*TODO*/ },
+            onClick = openBottomSheetClick,
             modifier = Modifier
                 .padding(16.dp)
         ) {
@@ -39,6 +43,6 @@ fun ButtonAdicionar() {
 @Preview
 @Composable
 fun ButtonAdicionarPrev() {
-    ButtonAdicionar()
+    ButtonAdicionar({  })
 
 }
