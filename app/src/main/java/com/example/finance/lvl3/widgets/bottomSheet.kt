3f1a31ac51099da455618891f3a595dfe9f58a2a
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-04-20 14:54:48 -0300

    Adiciona uma maneira de scollar o formulário de movimentação dentro do bottomSheet

--- app/src/main/java/com/example/finance/lvl3/widgets/bottomSheet.kt
+++ app/src/main/java/com/example/finance/lvl3/widgets/bottomSheet.kt
@@ -3,6 +3,12 @@ package com.example.finance.lvl3.widgets
 import FormularioMovimentacao
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -13,7 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.finance.lvl1.Login
 import com.example.finance.lvl1.MovimentacaoHolder
 import com.example.finance.lvl1.Pessoa
@@ -36,11 +44,17 @@ fun BottomSheet(
             sheetState = rememberModalBottomSheetState(),
             onDismissRequest = onDismiss
         ) {
-            FormularioMovimentacao(
-                onDismiss,
-                membroSelecionado = membroSelecionado,
-                onConfirm = onConfirmFormulario
-            )
+            Column (
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+            ){
+                FormularioMovimentacao(
+                    onDismiss,
+                    membroSelecionado = membroSelecionado,
+                    onConfirm = onConfirmFormulario
+                )
+            }
+
         }
     }
 
