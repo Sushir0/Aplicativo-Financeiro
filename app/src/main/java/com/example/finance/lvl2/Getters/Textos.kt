0eaef2d30b4fc6168865497a92eb2eec915fb440
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-05-03 19:36:52 -0300

    cria funções de segundo nível para trabalhar como meios para acessar classes inferiores

--- app/src/main/java/com/example/finance/lvl2/Getters/Textos.kt
+++ app/src/main/java/com/example/finance/lvl2/Getters/Textos.kt
@@ -4,7 +4,9 @@ import com.example.finance.lvl1.Login
 import com.example.finance.lvl1.Movimentacao
 import com.example.finance.lvl1.MovimentacaoHolder
 import com.example.finance.lvl1.Periodo
-import com.example.finance.lvl1.getPeriodoFromDatasUtilizadas
+import com.example.finance.lvl1.getPeriodosFromMovimentacoes
+import com.example.finance.lvl1.getUltimoPeriodoUtilizado
+import com.example.finance.lvl3.widgets.Tipo
 
 fun getMembros(): ArrayList<MovimentacaoHolder> {
     val membros =  ArrayList<MovimentacaoHolder>()
@@ -13,8 +15,35 @@ fun getMembros(): ArrayList<MovimentacaoHolder> {
     return membros
 }
 
+fun getMembroById(id:Int):MovimentacaoHolder{
+    var membros =  getMembros()
+    membros.forEach{
+        if (it.sameID(id)){
+            return it
+        }
+    }
+    return Login.getCasaLogada()
+}
+
+
 fun getPeriodos(movimentacoes: List<Movimentacao>): ArrayList<Periodo>{
     val periodos = ArrayList<Periodo>()
-    periodos.addAll(getPeriodoFromDatasUtilizadas(movimentacoes))
+    periodos.addAll(getPeriodosFromMovimentacoes(movimentacoes))
     return periodos
+}
+
+fun getPeriodoInicial(membroSelecionado: MovimentacaoHolder): Periodo{
+    return getUltimoPeriodoUtilizado(
+        getPeriodosFromMovimentacoes(
+            membroSelecionado.movimentacoes
+        )
+    )
+}
+
+fun getMovimentacoes(movimentacaoHolder: MovimentacaoHolder, isGasto: Boolean, periodo: Periodo):List<Movimentacao>{
+    return if (isGasto){
+        movimentacaoHolder.getGastos(periodo)
+    }else{
+        movimentacaoHolder.getRecebimentos(periodo)
+    }
 }
