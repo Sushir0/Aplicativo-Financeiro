e639535a99455d63dd9d19f1c940676a901bf283
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-05-03 19:28:03 -0300

    modifica a maneira que os detalhes era implementados, agora como classe e não somente uma função

---
+++ app/src/main/java/com/example/finance/lvl3/activitys/DetalhesActivity.kt
@@ -0,0 +1,207 @@
+package com.example.finance.lvl3.activitys
+
+import FormularioMovimentacao
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import com.example.finance.lvl1.Login
+import com.example.finance.lvl1.MovimentacaoHolder
+import com.example.finance.lvl1.Periodo
+import com.example.finance.lvl1.gerarCategoriasBasicas
+import com.example.finance.lvl1.getPeriodoFromNome
+import com.example.finance.lvl1.getPeriodosFromMovimentacoes
+import com.example.finance.lvl1.getUltimoPeriodoUtilizado
+import com.example.finance.lvl2.Getters.getMembroById
+import com.example.finance.lvl2.Getters.getMovimentacoes
+import com.example.finance.lvl2.Login.testeCadastro
+import com.example.finance.lvl2.Movimentacao.testeAdicionarMovimentacao
+import com.example.finance.lvl3.componentes.ItemSaldo
+import com.example.finance.lvl3.componentes.listas.NewListaDeMovimentacao
+import com.example.finance.lvl3.layouts.Footer
+import com.example.finance.lvl3.layouts.Header
+import com.example.finance.lvl3.telas.Detalhes
+import com.example.finance.lvl3.widgets.BottomSheet
+import com.example.finance.lvl3.widgets.DropdownIsGasto
+import com.example.finance.lvl3.widgets.Tipo
+import com.example.finance.ui.theme.FinanceTheme
+import com.example.finance.ui.theme.backgroundDark
+import com.example.finance.ui.theme.backgroundLight
+
+var detalhes: Detalhes? = null
+
+class DetalhesActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val movimentacaoHolder = intent.getSerializableExtra("movimentacaoHolder") as MovimentacaoHolder
+        val isGasto :Boolean = intent.getBooleanExtra("isGasto", false)
+        val periodo: Periodo = intent.getSerializableExtra("periodo") as Periodo
+        detalhes = Detalhes(
+            movimentacaoHolder = movimentacaoHolder,
+            isGastoInicial = isGasto,
+            periodoInicial = periodo
+        )
+        setContent {
+            FinanceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    detalhes!!.content()
+                }
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Detalhes(
+    movimentacaoHolder: MovimentacaoHolder,
+    isGastoInicial: Boolean,
+    ) {
+    val membroSelecionado = remember {
+        mutableStateOf<MovimentacaoHolder>(getMembroById(movimentacaoHolder.id))
+    }
+
+    val periodoSelecionado = remember{
+        mutableStateOf(
+            getUltimoPeriodoUtilizado(
+                getPeriodosFromMovimentacoes(
+                    movimentacaoHolder.movimentacoes
+                )
+            )
+        )
+    }
+    var periodosUtilizados by remember {
+        mutableStateOf(getPeriodosFromMovimentacoes(membroSelecionado.value.movimentacoes))
+    }
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+
+    var background = if(isSystemInDarkTheme()){
+        backgroundDark
+    }else{
+        backgroundLight
+    }
+    var tipoSelecionado by remember {
+        mutableStateOf(Tipo.getTipoFromIsGasto(isGastoInicial))
+    }
+
+    val movimentacoes by remember{
+        mutableStateOf(
+            getMovimentacoes(
+                movimentacaoHolder = membroSelecionado.value,
+                periodo = periodoSelecionado.value,
+                isGasto = false
+            )
+        )
+    }
+
+    
+    fun atualizar() {
+        periodosUtilizados = getPeriodosFromMovimentacoes(membroSelecionado.value.movimentacoes)
+        periodoSelecionado.value = getPeriodoFromNome(membroSelecionado.value.nome, periodosUtilizados) ?: getUltimoPeriodoUtilizado(periodosUtilizados)
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(background)
+            .padding(bottom = 64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Header(nome = membroSelecionado.value.nome)
+        ItemSaldo(valor = membroSelecionado.value.getSaldo(periodoSelecionado.value))
+        DropdownIsGasto(
+            tipoSelecionado = tipoSelecionado,
+            modifier = Modifier
+                .fillMaxWidth(.75f)
+                .padding(
+                    start = 4.dp,
+                    top = 4.dp
+                )
+        ) {
+            tipoSelecionado = it
+        }
+        NewListaDeMovimentacao(
+            movimentacoes = movimentacoes,
+            isAlways = tipoSelecionado == Tipo.TODOS
+        )
+
+    }
+
+    Footer(
+        periodosUtilizados = periodosUtilizados,
+        openBottomSheetClick = { isSheetOpen = true },
+        periodoSelecionado =    periodoSelecionado.value,
+        onChoicePeriodo = { periodoSelecionado.value = it }
+    )
+
+    BottomSheet(
+        isSheetOpen = isSheetOpen,
+        onDismiss = { isSheetOpen = false }){
+        FormularioMovimentacao(
+            membroPreSelecionado = membroSelecionado.value,
+            onConfirm = {
+                atualizar()
+            },
+            lockedMembro = true
+        )
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+private fun DetalhesPreview() {
+    testeCadastro()
+    gerarCategoriasBasicas()
+    testeAdicionarMovimentacao(Login.getCasaLogada())
+    val detalhes = Detalhes(
+        movimentacaoHolder = Login.getCasaLogada(),
+        isGastoInicial = true)
+    FinanceTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Detalhes(movimentacaoHolder = Login.getCasaLogada(), isGastoInicial = true)
+        }
+    }
+}
+
+fun abrirDetalhes(context: Context, movimentacaoHolder: MovimentacaoHolder, isGasto: Boolean, periodo: Periodo){
+    val intent = Intent(context, DetalhesActivity::class.java).apply {
+        putExtra("movimentacaoHolder", movimentacaoHolder)
+        putExtra("isGasto", isGasto)
+        putExtra("periodo", periodo)
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    }
+    ActivityCompat.startActivity(context, intent, null)
+}
---
+++ app/src/main/java/com/example/finance/lvl3/telas/Detalhes.kt
@@ -0,0 +1,149 @@
+package com.example.finance.lvl3.telas
+
+import FormularioMovimentacao
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.finance.lvl1.MovimentacaoHolder
+import com.example.finance.lvl1.Periodo
+import com.example.finance.lvl1.getMovimentacaoHolderById
+import com.example.finance.lvl1.getPeriodoFromNome
+import com.example.finance.lvl1.getPeriodosFromMovimentacoes
+import com.example.finance.lvl1.getUltimoPeriodoUtilizado
+import com.example.finance.lvl2.Getters.getMovimentacoes
+import com.example.finance.lvl3.componentes.ItemSaldo
+import com.example.finance.lvl3.componentes.listas.NewListaDeMovimentacao
+import com.example.finance.lvl3.layouts.Footer
+import com.example.finance.lvl3.layouts.Header
+import com.example.finance.lvl3.utils.avisoLongo
+import com.example.finance.lvl3.widgets.BottomSheet
+import com.example.finance.lvl3.widgets.DropdownIsGasto
+import com.example.finance.lvl3.widgets.Tipo
+import com.example.finance.ui.theme.backgroundDark
+import com.example.finance.ui.theme.backgroundLight
+
+class Detalhes (
+    private val movimentacaoHolder: MovimentacaoHolder,
+    private val isGastoInicial: Boolean,
+    private val periodoInicial: Periodo
+){
+    private val membroSelecionado: MovimentacaoHolder by mutableStateOf(getMovimentacaoHolderById(movimentacaoHolder.id))
+    private var periodosUtilizados by mutableStateOf(getPeriodosFromMovimentacoes(membroSelecionado.movimentacoes))
+    private var periodoSelecionado by mutableStateOf(getPeriodoFromNome(periodoInicial.nome, periodosUtilizados)?: periodoInicial)
+    private var movimentacoes by mutableStateOf(
+            getMovimentacoes(
+                movimentacaoHolder = membroSelecionado,
+                periodo = periodoSelecionado,
+                isGasto = isGastoInicial
+            )
+        )
+    private var tipoSelecionado by mutableStateOf(Tipo.getTipoFromIsGasto(isGastoInicial))
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Composable
+    fun content(){
+        val context = LocalContext.current
+        var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+        var background = if(isSystemInDarkTheme()){
+            backgroundDark
+        }else{
+            backgroundLight
+        }
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(background)
+                .padding(bottom = 64.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Header(nome = membroSelecionado.nome)
+            ItemSaldo(valor = membroSelecionado.getSaldo(periodoSelecionado))
+            DropdownIsGasto(
+                tipoSelecionado = tipoSelecionado,
+                modifier = Modifier
+                    .fillMaxWidth(.75f)
+                    .padding(
+                        start = 4.dp,
+                        top = 4.dp
+                    )
+            ) {
+                tipoSelecionado = it
+                atualizar(context)
+            }
+            NewListaDeMovimentacao(
+                movimentacoes = movimentacoes,
+                isAlways = tipoSelecionado == Tipo.TODOS
+            )
+
+        }
+
+        Footer(
+            periodosUtilizados = periodosUtilizados,
+            openBottomSheetClick = { isSheetOpen = true },
+            periodoSelecionado =    periodoSelecionado,
+            onChoicePeriodo = {
+                periodoSelecionado = it
+                atualizar(context)
+            }
+        )
+
+        BottomSheet(
+            isSheetOpen = isSheetOpen,
+            onDismiss = { isSheetOpen = false }){
+            FormularioMovimentacao(
+                membroPreSelecionado = membroSelecionado,
+                onConfirm = {
+                    atualizar(context)
+                },
+                lockedMembro = true,
+                onDismiss = { isSheetOpen = false }
+            )
+        }
+
+    }
+
+    fun atualizar(context: Context) {
+        periodosUtilizados = getPeriodosFromMovimentacoes(membroSelecionado.movimentacoes)
+        periodoSelecionado = getPeriodoFromNome(periodoSelecionado.nome, periodosUtilizados) ?: getUltimoPeriodoUtilizado(periodosUtilizados)
+        movimentacoes = when(tipoSelecionado){
+            Tipo.GASTO -> membroSelecionado.getGastos(periodoSelecionado)
+            Tipo.RECEBIMENTO -> membroSelecionado.getRecebimentos(periodoSelecionado)
+            Tipo.TODOS -> membroSelecionado.getMovimentacoes(periodoSelecionado)
+        }
+        avisoLongo(context, periodoSelecionado.nome)
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
