66935bc5c5e211da5948662bf28a7bf418a0a002
Ciro Roberto <94091123+Sushir0@users.noreply.github.com>
2024-05-04 11:00:43 -0300

    padroniza a utilização dos dropdowns

--- app/src/main/java/com/example/finance/lvl3/widgets/DropdownCategoria.kt
+++ app/src/main/java/com/example/finance/lvl3/widgets/DropdownCategoria.kt
@@ -1,6 +1,7 @@
 package com.example.finance.lvl3.widgets
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,25 +9,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.finance.lvl1.Categoria
-import com.example.finance.lvl1.categorias
+import com.example.finance.lvl1.Login
 import com.example.finance.lvl1.gerarCategoriasBasicas
 import com.example.finance.ui.theme.FinanceTheme
-import com.example.finance.ui.theme.backgroundDark
-import com.example.finance.ui.theme.backgroundLight
 
 
 
@@ -34,22 +40,21 @@ import com.example.finance.ui.theme.backgroundLight
 fun DropdownCategoria(
     expandedInicial : Boolean = false,
     categorias: List<Categoria>,
-    categoriaSelecionada : MutableState<Categoria?>,
-    modifier: Modifier = Modifier
+    categoriaSelecionada : Categoria?,
+    modifier: Modifier = Modifier,
+    onChoice: (Categoria) -> Unit
 ) {
-    var expandedMenu = remember { mutableStateOf<Boolean>(expandedInicial) }
-    var textoBotao = if(categoriaSelecionada.value == null){
-        "Selecione uma categoria."
-    }else{
-        categoriaSelecionada.value!!.nome
-    }
+    var expandedMenu by remember { mutableStateOf<Boolean>(expandedInicial) }
+    var textoBotao = categoriaSelecionada?.nome ?: "Selecione uma categoria."
+
+    var categoriasFiltradas by remember { mutableStateOf(categorias) }
 
 
     Column(modifier = modifier) {
         OutlinedButton(
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(4.dp),
-            onClick = { expandedMenu.value = true },
+            onClick = { expandedMenu = true },
         ) {
             Text(
                 text = textoBotao,
@@ -58,18 +63,20 @@ fun DropdownCategoria(
 
         }
         DropdownMenu(
-            expanded = expandedMenu.value,
+            expanded = expandedMenu,
             onDismissRequest = {
-                expandedMenu.value = false
+                expandedMenu = false
             },
             modifier = modifier
 
         ) {
-            categorias.forEach { categoria ->
+            categoriasFiltradas.forEach { categoria ->
                 DropdownItem(
                     categoria = categoria,
-                    expanded = expandedMenu,
-                    categoriaSelecionada = categoriaSelecionada
+                    onChoice = {
+                        onChoice(it)
+                        expandedMenu = false
+                    }
                 )
             }
         }
@@ -80,8 +87,7 @@ fun DropdownCategoria(
 @Composable
 private fun DropdownItem(
     categoria : Categoria,
-    expanded: MutableState<Boolean>,
-    categoriaSelecionada: MutableState<Categoria?>
+    onChoice: (Categoria) -> Unit = {},
 ) {
     DropdownMenuItem(
         text = { Text(
@@ -89,22 +95,29 @@ private fun DropdownItem(
             style = MaterialTheme.typography.titleSmall,
         ) },
         onClick = {
-            categoriaSelecionada.value = categoria
-            expanded.value = false
+            onChoice(categoria)
         },
     )
 }
-
 
 @Preview
 @Composable
 private fun DropdownCategoriaPrev() {
     FinanceTheme {
-        gerarCategoriasBasicas()
-        var categoriaSelecionada = remember { mutableStateOf<Categoria?>(null) }
+        var flag by remember {
+            mutableStateOf(true)
+        }
+        if (flag){
+            gerarCategoriasBasicas()
+            flag = false
+        }
+        var categoriaSelecionada by remember { mutableStateOf<Categoria?>(null) }
         DropdownCategoria(
-            categorias = categorias,
+            categorias = Login.getCasaLogada().getCategorias(),
             categoriaSelecionada = categoriaSelecionada,
+            onChoice = {
+                categoriaSelecionada = it
+            }
         )
     }
 }
